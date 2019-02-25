4fe9cffae220cb25a71273a463c46c66956803b2
smorebrod <36259392+smorebrod@users.noreply.github.com>
2019-02-25 12:14:46 +0100

    bil + MC

---
+++ Billetlibary/Billetlibary/MC.cs
@@ -0,0 +1,20 @@
+﻿namespace Billetlibary
+{
+    public class MC
+    {
+        public string Nummerplade { get; set; }
+
+        public DateTime dato { get; set; }
+
+        public decimal pris()
+        {
+            return 125;
+
+        }
+
+        public string køretøj()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ Billetlibary/Billetlibary/bil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Billetlibary
+{
+    public class bil
+    {
+        public string Nummerplade { get; set; }
+
+        public DateTime Dato { get; set; }
+
+
+        public decimal Pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+    }
+}
